68225b1b07348f730d5a478e016ad271087ee0c4
rudghks373 <naver.com>
2021-12-07 00:11:53 +0900

    연동 테스트

--- src/bj5543.java
+++ src/bj5543.java
@@ -7,8 +7,6 @@ public class bj5543 {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		int arr[] = new int[5];
-		
-		
 		for(int i =0; i < arr.length; i++) {
 			arr[i] = sc.nextInt();
 		}
